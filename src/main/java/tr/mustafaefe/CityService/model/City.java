985c7c8387ad89d67c55cb76a8c3e21fe595f342
mustafeefe15 <m.efe_1905@hotmail.com>
2022-02-28 21:14:33 +0300

    Bug fixed.

--- src/main/java/tr/mustafaefe/CityService/model/City.java
+++ src/main/java/tr/mustafaefe/CityService/model/City.java
@@ -10,8 +10,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class City {
 
 	@JsonView(City.class)
